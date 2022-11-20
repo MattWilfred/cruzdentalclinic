1680c52a5b77bc1949919d9d895911d04640d856
Matt Wilfred Cabunoc Salvador <kuya@Ds-MacBook-Pro.local>
2022-11-21 04:46:31 +0800

    asas

--- Database/connect.php
+++ Database/connect.php
@@ -1,7 +1,7 @@
 <?php
 //created by Bryan Joshua Bucu
 	//connect to database
-	$host = "0.0.0.0:3306";
+	$host = "0.0.0.0";
 	$user = "cruzdentalclinic";
 	$password = "";
 	$database = 'cruzdentalclinic';
