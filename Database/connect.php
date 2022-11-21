eb4d4abdcafcb9d9de20a99260b6a9d0a44358ce
Matt Wilfred Cabunoc Salvador <kuya@Ds-MacBook-Pro.local>
2022-11-21 14:13:26 +0800

    assda

--- Database/connect.php
+++ Database/connect.php
@@ -1,7 +1,7 @@
 <?php
 //created by Bryan Joshua Bucu
 	//connect to database
-	$host = "0.0.0.0";
+	$host = "127.0.1.1";
 	$user = "cruzdentalclinic";
 	$password = "";
 	$database = 'cruzdentalclinic';
