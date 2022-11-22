fd19951f8b17108c3df8cce30163efb4a6625f04
Matt Wilfred Cabunoc Salvador <kuya@Ds-MacBook-Pro.local>
2022-11-22 16:47:12 +0800

    asd

--- Database/validatelogin.php
+++ Database/validatelogin.php
@@ -1,18 +1,19 @@
 <?php
 //created by Bryan Joshua Bucu
 
-//include __DIR__ . ('connect.php');  
-require ('connect.php');
+include __DIR__ . ('/connect.php');  
+require ('/connect.php');
 session_start();
 
 /*
  check if user is already  already login
  if user is alreadly login redirect user to index page.
 */
-if($_SESSION['loggedIn'] == 'true'){ 
+
+/**if($_SESSION['loggedIn'] == 'true'){ 
      //echo  "<script> window.location='index.php'; </script>";
      echo  "<script> window.location='/index.html'; </script>";
-}
+}**/
 
 //if user clicked login button then execute code below. 
 if(isset($_POST['login'])){
