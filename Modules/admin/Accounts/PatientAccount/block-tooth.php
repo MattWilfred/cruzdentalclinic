c9cfc567acc2d1c07d370ff5dc2e9d4fde713c0b
Matt Wilfred Cabunoc Salvador <kuya@Ds-MacBook-Pro.local>
2022-11-24 21:01:26 +0800

    as

--- Modules/admin/Accounts/PatientAccount/block-tooth.php
+++ Modules/admin/Accounts/PatientAccount/block-tooth.php
@@ -6,7 +6,7 @@ require ("$_SERVER[DOCUMENT_ROOT]/Database/connect.php");
 function checkIfMissing($toothNum, $uid){
     global $connection;
 
-    $ifMissing = mysqli_query($connection, "SELECT findings FROM diagnosis WHERE tooth_number = $toothNum AND findings = 'Missing' AND id='$uid'");
+    $ifMissing = mysqli_query($connection, "SELECT findings FROM diagnosis WHERE tooth_number = $toothNum AND findings = 'Missing' AND userid='$uid'");
 
     if(mysqli_num_rows($ifMissing) > 0){
         echo "disabled";
@@ -16,7 +16,7 @@ function checkIfMissing($toothNum, $uid){
 function colorIfMissing($toothNum, $uid){
     global $connection;
 
-    $ifMissing = mysqli_query($connection, "SELECT findings FROM diagnosis WHERE tooth_number = $toothNum AND findings = 'Missing' AND id='$uid'");
+    $ifMissing = mysqli_query($connection, "SELECT findings FROM diagnosis WHERE tooth_number = $toothNum AND findings = 'Missing' AND userid='$uid'");
 
     if(mysqli_num_rows($ifMissing) > 0){
         echo "style= 'filter: grayscale(1000%)'";
