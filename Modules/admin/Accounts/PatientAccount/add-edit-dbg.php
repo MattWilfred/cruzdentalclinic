ec3a74a115a19dc5d6c828bd1f65e065207f2d74
Matt Wilfred Cabunoc Salvador <kuya@Ds-MacBook-Pro.local>
2022-11-24 21:03:44 +0800

    as

--- Modules/admin/Accounts/PatientAccount/add-edit-dbg.php
+++ Modules/admin/Accounts/PatientAccount/add-edit-dbg.php
@@ -42,7 +42,7 @@ if(isset($_POST['submit'])){
 
     $query = "INSERT INTO dentalbackground VALUES (DEFAULT,'$uid','$cb1','$cb2','$cb3','$cb4','$cb5','$cb6','$cb7','$cb8','$cb9',
     '$cb10','$cb11','$cb12','$cb13','$cb14','$cb15','$cb16','$cb17','$cb18','$cb19','$cb20',
-    '$cb21','$cb22','$cb23','$cb24','$cb25','$cb26')";
+    '$cb21','$cb22','$cb23','$cb24','$cb25','$cb26', NOW())";
 
     $query_run = mysqli_query($connection, $query);
 
