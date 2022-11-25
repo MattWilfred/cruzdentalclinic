0d3a91ad7ca26b41f2ff1fd8857dc1b33bcc76c9
Matt Wilfred Cabunoc Salvador <kuya@Ds-MacBook-Pro.local>
2022-11-26 03:26:31 +0800

    asd

--- Modules/dentist/Accounts/PatientAccount/patientdiagnosis.php
+++ Modules/dentist/Accounts/PatientAccount/patientdiagnosis.php
@@ -49,12 +49,14 @@ require ("$_SERVER[DOCUMENT_ROOT]/Database/connect.php");
    
 
    <!--========== NAV ==========-->
-   <div class="nav" id="navbar">
+
+
+<div class="nav" id="navbar">
     <nav class="nav__container">
         <div>
             <a href="#" class="nav__link nav__logo">
            <i class='nav__icon'>
-           <img src="/Modules/admin/assets/img/logo dental.png" alt="" class="header__img">
+           <img src="/Modules/dentist/assets/img/logo dental.png" alt="" class="header__img">
            </i>
                 <span class="nav__logo-name">Cruz Dental Clinic</span>
             </a>
@@ -62,13 +64,13 @@ require ("$_SERVER[DOCUMENT_ROOT]/Database/connect.php");
             <div class="nav__list">
                 <div class="nav__items">
 
-                    <a href="/Modules/admin/index.php" class="nav__link active">
+                    <a href="/Modules/dentist/index.php" class="nav__link active">
                         <i class='bx bx-home nav__icon' ></i>
                         <span class="nav__name">Dashboard</span>
                     </a>
                     
                     <div class="nav__dropdown">
-                        <a href="#" class="nav__link">
+                        <a href="/Modules/dentist/index.php" class="nav__link">
                             <i class='bx bxs-calendar nav__icon' ></i>
                             
                             <span class="nav__name">Schedule</span>
@@ -77,8 +79,7 @@ require ("$_SERVER[DOCUMENT_ROOT]/Database/connect.php");
 
                         <div class="nav__dropdown-collapse">
                             <div class="nav__dropdown-content">
-                                <a href="/Modules/admin/php-calendar/selectdentist.php" class="nav__dropdown-item">Calendar</a>
-                                <a href="/Modules/admin/php-calendar/schedule-list.php" class="nav__dropdown-item">Schedule List</a>
+                                <a href="/Modules/dentist/php-calendar/schedule-list.php" class="nav__dropdown-item">Schedule List</a>
                                
                             </div>
                         </div>
@@ -93,22 +94,16 @@ require ("$_SERVER[DOCUMENT_ROOT]/Database/connect.php");
 
                         <div class="nav__dropdown-collapse">
                             <div class="nav__dropdown-content">
-                            <a href="/Modules/admin/Accounts/SecretaryAccount/index.php" class="nav__dropdown-item">Employees</a>
-                                <a href="/Modules/admin/Accounts/DentistAccount/index.php" class="nav__dropdown-item">Dentist</a>
-                                <a href="/Modules/admin/Accounts/PatientAccount/index.php" class="nav__dropdown-item">Patients</a>
+                                <a href="/Modules/dentist/Accounts/PatientAccount/index.php" class="nav__dropdown-item">Patients</a>
                                
                             </div>
                         </div>
                     </div>
 
+                  
+                  </div>
 
-                    <a href="/Modules/admin/billing/billing.php" class="nav__link">
-                        <i class='bx bx-money nav__icon' ></i>
-                        <span class="nav__name">Billing</span>
-                    </a>
-                </div>
-
-                <a href="/Modules/admin/announcement/announcement.php" class="nav__link">
+                <a href="/Modules/dentist/announcement/announcement.php" class="nav__link">
                     <i class='bx bxs-megaphone nav__icon'></i>
                     <span class="nav__name">Announcement</span>
                 </a>
